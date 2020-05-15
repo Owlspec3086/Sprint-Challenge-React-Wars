6595deee33c0e643f9a403efc31d0e47173f6fd2
Owlspec3086 <ethical.worker3@gmail.com>
2020-05-15 17:26:00 -0400

    Flex sprint

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import CCard from './components/CCard';
+import { Container, Row } from 'reactstrap';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [character, setCharacter] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then((response) => {
+        const charData = response.data.results;
+        console.log('Character Data', charData);
+        setCharacter(charData);
+      })
+      .catch((error) => {
+        console.log('Do you want to develop an app?', error);
+      });
+  }, []); //Dependency array
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className='App'>
+      <h1 className='Header'>Rick and Morty</h1>
+      <Container>
+        <Row>
+          {character.map((character, index) => {
+            //Where my character card will go
+            return (
+              <CCard
+                key={index}
+                name={character.name}
+                gender={character.gender}
+                location={character.location}
+                episode={character.episode}
+                image={character.image}
+              />
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,42 @@
 // Write your Character component here
+import React, { Component } from 'react';
+import './App.css';
+import CharCard from './components/CharCard';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rickandmortyChars: []
+    };
+  }
+
+  componentDidMount() {
+    this.getCharacters('https://rickandmortyapi.com/character/');
+  }
+
+  getCharacters = URL => {
+    //Fetching data
+    
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ rickandmorty: data.results });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="Header">Rick and Marty</h1>
+            <CCard rickandmortyChars = {this.state.rickandmorty} />
+      </div>
+    );
+  }
+}
+
+export default App;
